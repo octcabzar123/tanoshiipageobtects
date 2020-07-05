8f883763836bc02192518890a03b10e6cf5b6d74
Octavio Cabrales <octavio.cabrales@astrazeneca.com>
2020-07-04 21:54:24 -0500

    inicializando page objects con junit

---
+++ src/main/java/com/tanoshii/junitpageobjects/App.java
@@ -0,0 +1,13 @@
+package com.tanoshii.junitpageobjects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
